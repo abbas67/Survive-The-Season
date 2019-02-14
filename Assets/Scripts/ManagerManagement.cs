5130cfc3a7c7a1e7b4c453b1449c9950893b076c
abbas67 <androidabbas@googlemail.com>
2019-02-14 15:33:43 +0000

    Day 2
    Day 2

---
+++ Assets/Scripts/ManagerManagement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerManagement : MonoBehaviour {
+
+    public InputField NameInputBox;
+    public InputField NationalityInputBox;
+    public InputField AgeInputBox;
+
+    int age;
+    string nationality;
+    string name;
+    string team;
+    bool employed;
+    int stresslevel;
+    int reputation;
+
+
+    public ManagerManagement(int age, string nationality, string name, string team, bool emplyed, int stresslevel, int reputation)
+        {
+
+        age = 21;
+        name = "Manager McManager";
+        team = "team mcteamface";
+        employed = false;
+        stresslevel = 20;
+        reputation = 25; 
+
+        }
+
+
+    public void ManagerCreate()
+    {
+
+    
+
+
+    }
+
+
+
+}
